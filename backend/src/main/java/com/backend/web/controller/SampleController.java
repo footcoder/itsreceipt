a60b39f93104d3de80a1cfb4aafe31598aef93e6
Park MinKyu <niee@naver.com>
2018-04-01 18:15:21 +0900

    closed #2

--- backend/src/main/java/com/backend/web/controller/SampleController.java
+++ backend/src/main/java/com/backend/web/controller/SampleController.java
@@ -1,11 +1,7 @@
 package com.backend.web.controller;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.HashMap;
 
 @RestController
 public class SampleController {
@@ -13,10 +9,5 @@ public class SampleController {
     @GetMapping(value = "/hello")
     public String sampleController(){
         return "Hello World last";
-    }
-
-    @GetMapping(value = "/receipts")
-    public ResponseEntity receipts(){
-        return new ResponseEntity(new HashMap<String, String>(), HttpStatus.OK);
     }
 }
