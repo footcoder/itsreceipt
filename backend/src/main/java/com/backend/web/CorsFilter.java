9b6a0da23db8f2b87f8178980841da7a568607af
Park MinKyu <niee@naver.com>
2018-04-05 21:28:42 +0900

    cors update #6

--- backend/src/main/java/com/backend/web/CorsFilter.java
+++ backend/src/main/java/com/backend/web/CorsFilter.java
@@ -3,6 +3,7 @@ package com.backend.web;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
@@ -15,7 +16,7 @@ public class CorsFilter {
         return new WebMvcConfigurer(){
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/*")
+                registry.addMapping("/**")
                         .allowCredentials(false)
                         .allowedOrigins("*")
                         .allowedMethods("POST","GET","OPTIONS")
