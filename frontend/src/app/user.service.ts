2f3d71fa1ec1940c03d30d20d58ba94f49ef6741
Park MinKyu <niee@naver.com>
2018-04-22 21:14:05 +0900

    sign in update #4

--- frontend/src/app/user.service.ts
+++ frontend/src/app/user.service.ts
@@ -1,6 +1,6 @@
 import {Injectable} from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
-import {headersToString} from "selenium-webdriver/http";
+import {Router} from "@angular/router";
 
 /*const httpOptions = {
  headers: new HttpHeaders({'Content-Type': 'application/json'})
@@ -10,24 +10,51 @@ import {headersToString} from "selenium-webdriver/http";
 export class UserService {
 
   authenticated = false;
+  token:string;
 
-  constructor(private  http: HttpClient) {
+  constructor(private  http: HttpClient, private router:Router) {
   }
 
   signIn(data) {
-    let headers = new HttpHeaders();
-    headers.append("Authorization", "Basic " + btoa(data.username + ":" + data.password));
-    this.http.get(
-      'http://footcoder.niee.kr:8080/user/sign-in',
-      {headers: headers})
+    // let headers = new HttpHeaders();
+    // headers.append("Authorization", "Basic " + btoa(data.username + ":" + data.password));
+    this.http.post('http://footcoder.niee.kr:8080/user/sign-in',data)
       .subscribe(
-        result => alert(result),
+        (response:any) => {
+          if(response.status == 'T' && response.result.token != null){
+            this.authenticated = true;
+            this.token = response.token;
+            this.router.navigateByUrl('main');
+          }else if(response.status == 'F'){
+            alert(response.result.message);
+          }
+        },
         err => console.log(err),
         () => console.log('sign-in finished')
       );
+    // let headers = new HttpHeaders();
+    // headers.append("Authorization", "Basic " + btoa(data.username + ":" + data.password));
+    // this.http.get(
+    //   'http://footcoder.niee.kr:8080/user/sign-in',
+    //   {headers: headers})
+    //   .subscribe(
+    //     result => alert(result),
+    //     err => console.log(err),
+    //     () => console.log('sign-in finished')
+    //   );
   }
 
   signup(data) {
-    return this.http.post('http://footcoder.niee.kr:8080/user/sign-up', data);
+    this.http.post('http://footcoder.niee.kr:8080/user/sign-up', data).subscribe(
+      (response:any) => {
+        if(response.status == 'T'){
+          this.router.navigateByUrl('sign-in');
+        }else if(response.status == 'F'){
+          alert(response.result.message);
+        }
+      }, err => {
+        console.log(err)
+      }, () => console.log('signup finish')
+    );;
   }
 }
