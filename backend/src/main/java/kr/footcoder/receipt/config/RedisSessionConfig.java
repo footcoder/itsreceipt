ec1a988f94b596c0f14cff931929c648c8ea532c
jhlee <ljh3259@naver.com>
2018-05-15 18:28:38 +0900

    change RedisTemplate -> StringRedisTemplate

--- backend/src/main/java/kr/footcoder/receipt/config/RedisSessionConfig.java
+++ backend/src/main/java/kr/footcoder/receipt/config/RedisSessionConfig.java
@@ -7,9 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.GenericToStringSerializer;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.data.redis.core.StringRedisTemplate;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -32,17 +30,10 @@ public class RedisSessionConfig {
 	}
 
 	@Bean
-	public RedisTemplate<String, Object> userInfoRedisTemplate(){
-		RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+	public StringRedisTemplate userInfoRedisTemplate(){
+		StringRedisTemplate stringRedisTemplate = new StringRedisTemplate();
+		stringRedisTemplate.setConnectionFactory(connectionFactory());
+		return stringRedisTemplate;
 
-		redisTemplate.setKeySerializer(new StringRedisSerializer());
-		redisTemplate.setHashKeySerializer(new StringRedisSerializer());
-
-		redisTemplate.setValueSerializer(new GenericToStringSerializer<>(Object.class));
-		redisTemplate.setHashValueSerializer(new GenericToStringSerializer<>(Object.class));
-
-		redisTemplate.setConnectionFactory(connectionFactory());
-
-		return redisTemplate;
 	}
 }
